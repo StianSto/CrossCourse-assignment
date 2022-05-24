6aa787c68ef721bc9f008c17546be5719d145549
StianSto <stordalstian@gmail.com>
2022-05-24 11:17:57 +0200

    js nav
    removed css only nav and added js. test only.

---
+++ test.js
@@ -0,0 +1,16 @@
+const navMenuBtn = document.querySelector(".hamburger-menu_header");
+const navMenu = document.querySelector("#primary-nav");
+
+navMenuBtn.addEventListener("click", menuDropdown);
+
+function menuDropdown() {
+    const visible = navMenu.getAttribute("data-visible")
+    if(visible === "false") {
+        navMenu.setAttribute("data-visible", true);
+        navMenu.setAttribute("aria-expanded", true);
+    } else {
+        navMenu.setAttribute("data-visible", false);
+        navMenu.setAttribute("aria-expanded", false);
+    }
+}
+
