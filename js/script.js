2f9a46e09f9d27d5a245e9f3f984046bd7608b95
StianSto <stordalstian@gmail.com>
2022-05-25 05:25:43 +0200

    bar animation
    removed svg since i couldnt center its origin. added span elements in html instead, and adde animatin. works as intended

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const navMenuBtn = document.querySelector(".hamburger-menu_header");
+const menuBars = document.querySelectorAll(".bar");
 const navMenu = document.querySelector("#primary-nav");
-const bar1 = document.querySelector("#Path_23");
 
-navMenuBtn.addEventListener("click", menuDropdown);
+navMenuBtn.addEventListener("click", () => {
+    const visible = navMenu.getAttribute("data-visible");
+    console.log(2);
 
-function menuDropdown() {
-    const visible = navMenu.getAttribute("data-visible")
+    menuBars.forEach(bar => {
+        bar.classList.toggle("close")
+    });
+
     if(visible === "false") {
         navMenu.setAttribute("data-visible", true);
         navMenu.setAttribute("aria-expanded", true);
@@ -14,5 +18,5 @@ function menuDropdown() {
         navMenu.setAttribute("aria-expanded", false);
     }
 
-    bar1.classList.add = "bar1-cross"
-}
+});
+
