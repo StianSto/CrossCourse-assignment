65803a7c1d732cbc2ff5bdc790378480c1f08401
StianSto <stordalstian@gmail.com>
2022-05-28 16:42:52 +0200

    added array for products. worked on navbar

---
+++ js/index.js
@@ -0,0 +1,22 @@
+import {products} from '/js/components/products.js';
+console.log(products)
+
+const winterCollectionProductsList = document.querySelector(".product_list--wintercollection")
+const popularProductsList = document.querySelector(".product_list--popular")
+
+products.forEach(product => {
+    let html = `<li class="product">
+    <a href="product-specific.html"> 
+        <div class="product-item product-upright">
+            <img src="${product.img}" alt="${product.altDescr}" class="product_image">               
+            <div>
+                <h3>${product.brandName}</h3>
+                <h4>${product.productName}</h4>
+            </div>
+            <p class="price">$ ${product.price}</p>
+        </div>
+    </a>  
+</li>`;
+    winterCollectionProductsList.innerHTML += html;
+    popularProductsList.innerHTML += html;
+});
