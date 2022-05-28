ee95fff2bb73d32cac7293e93c7ad60eef05863a
StianSto <stordalstian@gmail.com>
2022-05-28 10:25:59 +0200

    made some semantic adjustments. moved font links higher up

---
+++ js/contactus-form.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".contact-us-form");
+const submitBtn = document.querySelector("#submit-btn");
+
+form.addEventListener("submit", (e) => {
+    // e.preventDefault();
+    console.log(2);
+})
