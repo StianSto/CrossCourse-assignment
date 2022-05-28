bbe6e8b5179bfa992aafbdf181bd9c53196c3d3f
StianSto <95475303+StianSto@users.noreply.github.com>
2022-05-29 00:33:16 +0200

    With javascript (#2)
    * applied new nav to all pages
    
    * added svg menubar for possible animation
    
    * bar animation
    
    removed svg since i couldnt center its origin. added span elements in html instead, and adde animatin. works as intended
    
    * added new navbar to all pages
    
    * cleaned up nav and hero
    
    * adde search aria label
    
    * nav animate
    
    fixed nav animation so it doesn't run on load
    
    * made form on contact page
    
    * cart icon, animation nav
    
    made a cart count style for when i need it. adjusted nav animation class
    
    * made some semantic adjustments. moved font links higher up
    
    * added array for products. worked on navbar
    
    * added hover pointer

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const navMenuBtn = document.querySelector(".hamburger-menu_header");
+const menuBars = document.querySelectorAll(".bar");
+const navMenu = document.querySelector("#primary-nav");
+const header = document.querySelector("header");
+
+navMenuBtn.addEventListener("click", () => {
+    const navIsVisible = navMenu.getAttribute("data-visible");
+    navMenu.classList.add("animate-nav")
+
+    menuBars.forEach(bar => {
+        bar.classList.toggle("close")
+    });
+
+    if(navIsVisible === "false") {
+        navMenu.setAttribute("data-visible", true);
+        navMenu.setAttribute("aria-expanded", true);
+    } else {
+        navMenu.setAttribute("data-visible", false);
+        navMenu.setAttribute("aria-expanded", false);
+    }
+
+    navMenu.addEventListener('transitionend', () => {
+        navMenu.classList.remove("animate-nav");
+    })
+});
+
+const collectionContainer = document.querySelector(".collection-container");
+const categoryContainer = document.querySelector(".container__category");
+collectionContainer.addEventListener("click", () => {
+    console.log(2);
+    collectionContainer.classList.toggle("collection-container--visible");
+    categoryContainer.classList.toggle("container__category--visible");
+}) 
+
+
+// this code is inpsired by w3schools : https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+var prevScrollpos = window.pageYOffset;
+// if (prevScrollpos 
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > "120") {
+        if (prevScrollpos > currentScrollPos) {
+            header.style.top = "0";
+        } else {
+            header.style.top = "-100%";
+        }
+    }
+    prevScrollpos = currentScrollPos;
+}
