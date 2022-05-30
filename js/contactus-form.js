809c064af55ca0b4de316d1cd6ad099fe587d45a
StianSto <95475303+StianSto@users.noreply.github.com>
2022-05-30 13:24:01 +0200

    With javascript (#3)
    * applied new nav to all pages
    
    * added svg menubar for possible animation
    
    * bar animation
    
    removed svg since i couldnt center its origin. added span elements in html instead, and adde animatin. works as intended
    
    * added new navbar to all pages
    
    * cleaned up nav and hero
    
    * adde search aria label
    
    * nav animate
    
    fixed nav animation so it doesn't run on load
    
    * made form on contact page
    
    * cart icon, animation nav
    
    made a cart count style for when i need it. adjusted nav animation class
    
    * made some semantic adjustments. moved font links higher up
    
    * added array for products. worked on navbar
    
    * added hover pointer
    
    * hero, prod-site, breadcrumbs, cative indicator, nav-style,
    
    * added to cart modal
    
    * isntalled hotjar

--- js/contactus-form.js
+++ js/contactus-form.js
@@ -2,6 +2,4 @@ const form = document.querySelector(".contact-us-form");
 const submitBtn = document.querySelector("#submit-btn");
 
 form.addEventListener("submit", (e) => {
-    // e.preventDefault();
-    console.log(2);
 })
--- js/script.js
+++ js/script.js
@@ -24,18 +24,10 @@ navMenuBtn.addEventListener("click", () => {
     })
 });
 
-const collectionContainer = document.querySelector(".collection-container");
-const categoryContainer = document.querySelector(".container__category");
-collectionContainer.addEventListener("click", () => {
-    console.log(2);
-    collectionContainer.classList.toggle("collection-container--visible");
-    categoryContainer.classList.toggle("container__category--visible");
-}) 
-
 
 // this code is inpsired by w3schools : https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
 var prevScrollpos = window.pageYOffset;
-// if (prevScrollpos 
+
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > "120") {
@@ -46,4 +38,33 @@ window.onscroll = function() {
         }
     }
     prevScrollpos = currentScrollPos;
+}
+
+const addToCartBtn = document.querySelector(".add-to-cart");
+addToCartBtn.addEventListener("click", addedToCart);
+
+function addedToCart() {
+    const addToCartModal = document.createElement("div");
+    addToCartModal.classList.add("add-to-cart__modal");
+    addToCartModal.innerHTML = `
+        <img src="/Assets/images/product_1.jpg" alt="">
+        <div class="add-to-cart__modal--text">
+            <p>Item has been added to your cart</p>
+            <button class="cta"><a href="/mycart.html">go to cart</a></button>
+        </div>
+    `
+    const contentWrapper = document.querySelector(".content_wrapper");
+    contentWrapper.appendChild(addToCartModal);
+
+    setTimeout(() => {
+        addToCartModal.style.transform = "translateX(0)"
+
+        setTimeout(() => {
+            addToCartModal.style.transform = "translateX(200%)"
+
+            setTimeout(() => {
+                addToCartModal.remove();
+            }, 5001);
+        }, 5000);
+    }, 1);
 }
