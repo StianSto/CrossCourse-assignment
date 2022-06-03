ec70288be0e731e929c2740da4f20a8e975dc262
StianSto <stordalstian@gmail.com>
2022-06-03 04:05:38 +0200

    finished up form validation on checkout
    added indicators for validated fieldsets. made it so it doesn't validate when first opened so user wont get annoyed

--- js/mycart.js
+++ js/mycart.js
@@ -1,7 +1,9 @@
 import {products} from '/js/components/products.js';
 import {updateCartCount} from '/js/script.js';
 
-const myCartForm = document.querySelector("form")
+const myCartForm = document.querySelector("form");
+myCartForm.onsubmit = function(e) {e.preventDefault();};
+
 const cartProductContainer = document.querySelector(".products_cart_flex");
 
 let cartItems = [];
@@ -102,13 +104,19 @@ headerCollapseable.forEach(i => {
 
 // continue to next fieldset
 function nextForm(e) {
-    console.log(e)
+
     let targetCollapseDataId = parseFloat(e.target.dataset.nextCollapse) + 1;
 
     collapseable.forEach(i => {
         const parsedId = parseFloat(i.dataset.collapse);
+
+        if(parsedId === 2 && i.dataset.hasopened) {validateAddressInfo();}
+        if(parsedId === 3 && i.dataset.hasopened) {validateDeliveryInfo();}
+        if(parsedId === 4 && i.dataset.hasopened) {validatePayment();}
+
         if(parsedId === targetCollapseDataId) {
             i.classList.remove("collapse--box")
+            i.setAttribute('data-hasopened', true);
         } else {
             i.classList.add("collapse--box")
         }
@@ -122,8 +130,13 @@ function openFieldset(e) {
     collapseable.forEach(i => {
         const parsedId = parseFloat(i.dataset.collapse);
 
+        if(parsedId === 2 && i.dataset.hasopened) {validateAddressInfo();}
+        if(parsedId === 3 && i.dataset.hasopened) {validateDeliveryInfo();}
+        if(parsedId === 4 && i.dataset.hasopened) {validatePayment();}
+
         if(parsedId === targetCollapseDataId) {
             i.classList.remove("collapse--box");
+            i.setAttribute('data-hasopened', true);
         } else {
             i.classList.add("collapse--box");
         }
@@ -132,14 +145,96 @@ function openFieldset(e) {
 
 
 // form validations
+const firstName = document.querySelector("#first_name");
+const lastName = document.querySelector("#last_name");
+const address = document.querySelector("#adress");
+const city = document.querySelector("#city");
+const postalCode = document.querySelector("#postal_code");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+
+let customerAddressInfo = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    email: "",
+    phone: ""
+} 
+
+
+
+// validate Address Info
+const regexNumbersOnly = /^[0-9]+$/;
+const regexEmail = /\S+@\S+\.\S+/;
+
+function validateAddressInfo() {
+
+    let inputIsValid = true;
+
+    
+    
+    function validateInputExists(input) {
+        if(!input.value.length > 0) {
+            inputIsValid = false;
+            input.classList.add("inputValidationError");
+        }
+    }
+    function validateInputNumbersOnly(input) {
+        if(!regexNumbersOnly.test(input.value)) {
+            inputIsValid = false;
+            input.classList.add("inputValidationError");
+        }
+    }
+    function validateInputEmail(input) {
+        if(!regexEmail.test(input.value)) {
+            inputIsValid = false;
+            input.classList.add("inputValidationError");
+        }
+    }
+
+    firstName.classList.remove("inputValidationError");
+    lastName.classList.remove("inputValidationError");
+    address.classList.remove("inputValidationError");
+    city.classList.remove("inputValidationError");
+    postalCode.classList.remove("inputValidationError");
+    email.classList.remove("inputValidationError");
+    phone.classList.remove("inputValidationError");
+
+    validateInputExists(firstName);
+    validateInputExists(lastName);
+    validateInputExists(address);
+    validateInputExists(city);
+    validateInputExists(postalCode);
+    validateInputNumbersOnly(postalCode);
+    validateInputEmail(email)
+    validateInputNumbersOnly(phone);
+
+    firstName.addEventListener("blur", validateAddressInfo)
+    lastName.addEventListener("blur", validateAddressInfo)
+    address.addEventListener("blur", validateAddressInfo)
+    city.addEventListener("blur", validateAddressInfo)
+    postalCode.addEventListener("blur", validateAddressInfo)
+    email.addEventListener("blur", validateAddressInfo)
+    phone.addEventListener("blur", validateAddressInfo)
+
+    if (inputIsValid) {
+        const addressIsGood = document.querySelector('[data-headercollapse="2"] .fa');
+        addressIsGood.classList.add("fa-check")
+        return true;
+    }return false
+}
+
+// validate Delivery Info
+
 const sameAsAddress = document.querySelector("#same_as_adress");
+sameAsAddress.addEventListener("click", sameAsAddressChecked);
 const deliveryAddress = document.querySelector("#delivery-adress");
 const deliveryCity = document.querySelector("#delivery-city");
 const deliveryPostalCode = document.querySelector("#delivery-postal_code");
 
 let useAddressInfoForDelivery = false;
-
-sameAsAddress.addEventListener("click", sameAsAddressChecked)
 function sameAsAddressChecked() {
     if (sameAsAddress.checked === true) {
         deliveryAddress.disabled = true;
@@ -151,5 +246,65 @@ function sameAsAddressChecked() {
         deliveryCity.disabled = false;
         deliveryPostalCode.disabled = false;
         useAddressInfoForDelivery = false;
+    }
+    validateDeliveryInfo();
+}
+
+function validateDeliveryInfo() {
+    let deliveryInputIsValid = true;
+    const addressIsGood = document.querySelector('[data-headercollapse="3"] .fa');
+    addressIsGood.classList.remove("fa-check");
+
+    deliveryAddress.classList.remove("inputValidationError");
+    deliveryCity.classList.remove("inputValidationError");
+    deliveryPostalCode.classList.remove("inputValidationError");
+
+    deliveryAddress.addEventListener("blur", validateDeliveryInfo);
+    deliveryCity.addEventListener("blur", validateDeliveryInfo);
+    deliveryPostalCode.addEventListener("blur", validateDeliveryInfo);
+
+    function validateInputExists(input) {
+        if(!input.value.length > 0) {
+            deliveryInputIsValid = false;
+            input.classList.add("inputValidationError");
+        }
+    }
+    function validateInputNumbersOnly(input) {
+        if(!regexNumbersOnly.test(input.value)) {
+            deliveryInputIsValid = false;
+            input.classList.add("inputValidationError");
+        }
+    }
+
+    if(!useAddressInfoForDelivery) {
+        validateInputExists(deliveryAddress);
+        validateInputExists(deliveryCity);
+        validateInputNumbersOnly(deliveryPostalCode);
+    } else {
+        deliveryInputIsValid = useAddressInfoForDelivery;
+    }
+    
+    if(deliveryInputIsValid) {
+        addressIsGood.classList.add("fa-check");
+        return true;
+    }
+}
+
+
+//  validate payment 
+const paymentOptions = document.querySelectorAll("[type='radio']");
+paymentOptions.forEach(e => {e.addEventListener("click", validatePayment)})
+
+function validatePayment(){
+    let paymentSelected = false;
+    paymentOptions.forEach(option => {
+        if(option.checked) {
+            paymentSelected = true;
+            console.log(paymentSelected)
+        }
+    })
+    if(paymentSelected) {
+        const paymentIsGood = document.querySelector('[data-headercollapse="4"] .fa');
+        paymentIsGood.classList.add("fa-check");
     }
 }
